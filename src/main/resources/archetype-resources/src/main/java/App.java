55cb38ab9326c8b965af351c3c7e3f1db5d029a5
masonFei <994085848@qq.com>
2026-04-07 23:39:11 +0800

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.fxy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
