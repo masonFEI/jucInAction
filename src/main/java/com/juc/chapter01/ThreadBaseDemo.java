553a51744b23ab9da862710f3ca491037720ad62
masonFei <994085848@qq.com>
2026-04-08 23:29:29 +0800

    03-start线程开启C源码分析

---
+++ src/main/java/com/juc/chapter01/ThreadBaseDemo.java
@@ -0,0 +1,23 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter01;
+
+/**
+ * ThreadBaseDemo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-04-08 23:15
+ */
+public class ThreadBaseDemo {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+        }, "t1");
+
+        t1.start();
+    }
+
+}
