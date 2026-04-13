e29506cbb79a5063fc197942569a7cefcbd3e0e0
masonFei <994085848@qq.com>
2026-04-13 22:17:12 +0800

    12-CompletableFuture之get获取容易阻塞

---
+++ src/main/java/com/juc/chapter03/FutureAPIDemo.java
@@ -0,0 +1,46 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter03;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * FutureAPIDemo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-04-13 22:05
+ */
+public class FutureAPIDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        FutureTask<String> futureTask = new FutureTask<String>(() -> {
+            System.out.println(Thread.currentThread().getName() + " \t -----come in");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "task over";
+        });
+        new Thread(futureTask, "t1").start();
+
+
+//        System.out.println(futureTask.get());
+
+        System.out.println(Thread.currentThread().getName() + "\t ----- 忙其他任务了");
+
+        System.out.println(futureTask.get(3, TimeUnit.SECONDS));
+    }
+
+    /**
+     * 1.get容易导致阻塞，一般建议放在程序后面
+     * 2.假如我不愿意等待很长时间，我希望过时不候，可以自动离开
+     */
+
+}
---
+++ src/main/java/com/juc/chapter03/FutureThreadPoolDemo.java
@@ -0,0 +1,91 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter03;
+
+import java.util.concurrent.*;
+
+/**
+ * FutureThreadPoolDemo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-04-13 21:47
+ */
+public class FutureThreadPoolDemo {
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 3个任务，目前开启多个异步任务线程来处理，请问耗时多少？
+
+        long startTime = System.currentTimeMillis();
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+
+        FutureTask<String> futureTask1 = new FutureTask<>(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "task1 over";
+        });
+        threadPool.submit(futureTask1);
+
+        FutureTask<String> futureTask2 = new FutureTask<>(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "task2 over";
+        });
+        threadPool.submit(futureTask2);
+
+        FutureTask<String> futureTask3 = new FutureTask<>(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "task3 over";
+        });
+        threadPool.submit(futureTask3);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(futureTask1.get());
+        System.out.println(futureTask2.get());
+        System.out.println(futureTask3.get());
+        System.out.println("----costTime:" + (endTime - startTime) + " ms");
+
+        threadPool.shutdown();
+    }
+
+    private static void m1() {
+        // 3个任务，目前只有一个线程main来处理，请问耗时多少
+        long startTime = System.currentTimeMillis();
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            TimeUnit.MILLISECONDS.sleep(300);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            TimeUnit.MILLISECONDS.sleep(300);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("----costTime:" + (endTime - startTime) + " ms");
+
+        System.out.println(Thread.currentThread().getName() + "\t ----end");
+    }
+
+
+}
