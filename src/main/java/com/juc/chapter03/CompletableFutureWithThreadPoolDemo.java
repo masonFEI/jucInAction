2315c7ec3e3c5554feee03af531d198241c20575
masonFei <994085848@qq.com>
2026-04-22 23:56:40 +0800

    26-CompletableFuture之线程池运行选择

--- src/main/java/com/juc/chapter03/CompletableFutureWithThreadPoolDemo.java
+++ src/main/java/com/juc/chapter03/CompletableFutureWithThreadPoolDemo.java
@@ -4,6 +4,11 @@
  */
 package com.juc.chapter03;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * CompletableFutureWithThreadPoolDemo
  *
@@ -14,7 +19,140 @@ package com.juc.chapter03;
 public class CompletableFutureWithThreadPoolDemo {
 
     public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        // 只有第一个用的threadPool,剩下用的是main线程
+        // 处理的太快，系统优化切换原则
+        try {
+            CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+//                try {
+//                    TimeUnit.MILLISECONDS.sleep(20);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
 
+                System.out.println("1号任务" + "\t" + Thread.currentThread().getName());
+                return "abcd";
+            }, threadPool).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("2号任务" + "\t" + Thread.currentThread().getName());
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("3号任务" + "\t" + Thread.currentThread().getName());
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("4号任务" + "\t" + Thread.currentThread().getName());
+            });
+
+            System.out.println(completableFuture.get(2L, TimeUnit.SECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+
+    }
+
+
+    private void thenRunAsync() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        // 只有第一个用的threadPool，剩下用的是forkJoinPool.commonPool()线程池
+        // 因为 thenRunAsync另起炉灶了
+        try {
+            CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("1号任务" + "\t" + Thread.currentThread().getName());
+                return "abcd";
+            }, threadPool).thenRunAsync(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("2号任务" + "\t" + Thread.currentThread().getName());
+            }).thenRunAsync(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("3号任务" + "\t" + Thread.currentThread().getName());
+            }).thenRunAsync(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("4号任务" + "\t" + Thread.currentThread().getName());
+            });
+
+            System.out.println(completableFuture.get(2L, TimeUnit.SECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+
+    private void thenRun() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
+        try {
+            CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("1号任务" + "\t" + Thread.currentThread().getName());
+                return "abcd";
+            }, threadPool).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("2号任务" + "\t" + Thread.currentThread().getName());
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("3号任务" + "\t" + Thread.currentThread().getName());
+            }).thenRun(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("4号任务" + "\t" + Thread.currentThread().getName());
+            });
+
+            System.out.println(completableFuture.get(2L, TimeUnit.SECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
+        }
     }
 
 
