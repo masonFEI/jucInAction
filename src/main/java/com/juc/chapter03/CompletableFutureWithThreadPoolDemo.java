82aafdd02a97806d376580eb8abeae5e6d3d8147
masonFei <994085848@qq.com>
2026-04-22 09:13:28 +0800

    26-CompletableFuture之对线程池运行选择

---
+++ src/main/java/com/juc/chapter03/CompletableFutureWithThreadPoolDemo.java
@@ -0,0 +1,21 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter03;
+
+/**
+ * CompletableFutureWithThreadPoolDemo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-04-22 09:07
+ */
+public class CompletableFutureWithThreadPoolDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
