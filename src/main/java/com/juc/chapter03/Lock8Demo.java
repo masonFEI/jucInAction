776c93729b25a6c90d2849c139b3e9b891c779e8
masonFei <994085848@qq.com>
2026-04-27 09:07:52 +0800

    32-多线程锁之8锁案例原理解释

--- src/main/java/com/juc/chapter03/Lock8Demo.java
+++ src/main/java/com/juc/chapter03/Lock8Demo.java
@@ -16,12 +16,12 @@ import java.util.concurrent.TimeUnit;
 
 class Phone {// 资源类
 
-    public static synchronized void sendEmail() {
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public synchronized void sendEmail() {
+//        try {
+//            TimeUnit.SECONDS.sleep(3);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
 
         System.out.println("sendEmail");
     }
@@ -47,13 +47,31 @@ class Phone {// 资源类
  * 6. 有两个静态同步方法，有2部手机，请问先打印邮件还是短信？sendEmail
  * 7. 有1个静态同步方法，有1个普通同步方法，有1部手机，请问先打印邮件还是短信？sendSMS
  * 8. 有1个静态同步方法，有1个普通同步方法，有2部手机，请问先打印邮件还是短信？sendSMS
- *
+ * <p>
+ * <p>
+ * 笔记总结：
+ * 1-2：
+ * 一个对象里面如果有多个synchronized方法，某一时刻内，只要一个线程去调用其中的一个synchronized方法了，其他线程只能等待，换句话说，某一个时刻内，只能有唯一一个线程去访问这些synchronized方法。
+ * 锁的是当前对象this，被锁定后，其他线程都不能进入到当前对象的其他synchronized方法。
+ * <p>
+ * 3：
+ * 普通方法和同步锁无关，所以先打印hello
+ * <p>
+ * 4：
+ * 换成两个对象后，不是同一把锁了，情况立刻变化了，先打印短信
+ * <p>
+ * 5-6：
+ * 加个static后，锁的不是当前对象了，而是类对象了，所有的对象的这个静态同步方法用的都是同一把锁了，所以先打印邮件
+ * <p>
+ * 7-8：
+ * 静态同步方法与普通同步方法之间不存在竞态条件的。
+ * 一个是静态同步方法，一个是普通同步方法，锁的也是不同的对象了，所以先打印短信
  */
 public class Lock8Demo {
 
     public static void main(String[] args) {
         Phone phone = new Phone();
-        Phone phone2 = new Phone();
+//        Phone phone2 = new Phone();
 
         new Thread(() -> {
             phone.sendEmail();
@@ -67,9 +85,9 @@ public class Lock8Demo {
         }
 
         new Thread(() -> {
-//            phone.sendSMS();
+            phone.sendSMS();
 //            phone.hello();
-            phone2.sendSMS();
+//            phone2.sendSMS();
         }, "b").start();
 
     }
