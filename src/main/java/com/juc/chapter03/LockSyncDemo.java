f03ec5013f3357add591da7c7df70ead3155eabb
masonFei <994085848@qq.com>
2026-04-27 22:56:55 +0800

    33-多线程锁之synchronized字节码分析

---
+++ src/main/java/com/juc/chapter03/LockSyncDemo.java
@@ -0,0 +1,36 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter03;
+
+/**
+ * LockSyncDemo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-04-27 22:44
+ */
+public class LockSyncDemo {
+
+    Object object = new Object();
+
+    public void m1() {
+        synchronized (object) {
+            System.out.println("---- hello synchronized code block");
+        }
+    }
+
+    public synchronized void m2() {
+        System.out.println("---- hello synchronized m2");
+    }
+
+    public static synchronized void m3() {
+        System.out.println("---- hello synchronized m3");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
