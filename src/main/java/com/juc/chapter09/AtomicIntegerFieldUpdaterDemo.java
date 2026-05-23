e8d164cd8108c300a84d91eb5e2ed6b07588cdb4
masonFei <994085848@qq.com>
2026-05-23 23:14:42 +0800

    87-原子类之原子操作增强类理论入门

---
+++ src/main/java/com/juc/chapter09/AtomicIntegerFieldUpdaterDemo.java
@@ -0,0 +1,70 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter09;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+/**
+ * AtomicIntegerFieldUpdaterDemo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-05-23 22:13
+ */
+
+class BankAccount {// 资源类
+    String              bankName = "CCB";
+
+    // 1.更新的对象属性必需使用public volatile修饰符
+    public volatile int money    = 0;    // 钱数
+
+    public void add() {
+        money++;
+    }
+
+    // 2.因为对象的属性修改类型原子类都是抽象类，所以每次使用都必须
+    // 使用静态方法newUpdater()创建一个更新器，并且需要设置想要更新的类和属性
+    AtomicIntegerFieldUpdater<BankAccount> fieldUpdater = AtomicIntegerFieldUpdater.newUpdater(BankAccount.class, "money");
+
+    // 不加synchronized，保证高性能原子性，局部微创小手术
+    public void transMoney(BankAccount bankAccount) {
+        fieldUpdater.getAndIncrement(bankAccount);
+    }
+
+}
+
+/**
+ * 以一种线程安全的方式操作非线程安全对象的某些字段
+ * 需求：
+ * 10个线程，
+ * 每个线程转账1000，
+ * 不使用synchronized,尝试使用AtomicIntegerFieldUpdater来实现线程安全的转账操作
+ */
+public class AtomicIntegerFieldUpdaterDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount bankAccount = new BankAccount();
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                try {
+                    for (int j = 0; j < 1000; j++) {
+                        // bankAccount.add();
+                        bankAccount.transMoney(bankAccount);
+                    }
+                } finally {
+                    countDownLatch.countDown();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        countDownLatch.await();
+
+        System.out.println(Thread.currentThread().getName() + "\t " + "result:" + bankAccount.money);
+    }
+
+}
---
+++ src/main/java/com/juc/chapter09/AtomicReferenceFieldUpdaterDemo.java
@@ -0,0 +1,54 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter09;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+class MyVar {
+    public volatile Boolean                     isInit                = false;
+
+    AtomicReferenceFieldUpdater<MyVar, Boolean> referenceFieldUpdater = AtomicReferenceFieldUpdater.newUpdater(MyVar.class, Boolean.class, "isInit");
+
+    public void init(MyVar myVar) {
+        if (referenceFieldUpdater.compareAndSet(myVar, Boolean.FALSE, Boolean.TRUE)) {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---- start init,need 3 seconds");
+
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(Thread.currentThread().getName() + "\t" + "---- over init");
+        } else {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---- 已经有线程在进行初始化工作了");
+        }
+
+    }
+}
+
+/**
+ * 多线程并发调用一个类的初始化方法，如果未被初始化过，将执行初始化工作，
+ * 要求只能被初始化一次，只有一个线程操作成功。
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-05-23 22:44
+ */
+public class AtomicReferenceFieldUpdaterDemo {
+
+    public static void main(String[] args) {
+        MyVar myVar = new MyVar();
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                myVar.init(myVar);
+            }, String.valueOf(i)).start();
+        }
+
+    }
+
+}
