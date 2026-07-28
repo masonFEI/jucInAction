de06174f6f0c00d2ebd9727b88bc234b892d37b0
masonFei <994085848@qq.com>
2026-07-28 22:32:03 +0800

    162-StampedLock锁之特点

---
+++ src/main/java/com/juc/chapter13/StampedLockDemo.java
@@ -0,0 +1,20 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package com.juc.chapter13;
+
+/**
+ * StampedLock = ReentrantReadWriteLock +读的过程中也允许获取写锁接入
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-07-28 22:30
+ */
+public class StampedLockDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
